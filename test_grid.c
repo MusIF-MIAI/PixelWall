0c09bc6398fbc5de71b82fada0a604378168e426
mancausoft <mancausoft@Macmini.casa>
2025-01-23 14:21:22 +0100

    add configuration on test grid

--- test_grid.c
+++ test_grid.c
@@ -16,7 +16,8 @@ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
 #include <stdio.h>   // Standard I/O functions
 #include <stdlib.h>  // Standard library functions
-#include <signal.h>  // Add this for signal handling
+#include <signal.h>  // Signal handling
+#include <unistd.h>  // POSIX API for getopt
 #include "raylib.h"  // Raylib graphics library
 
 // Default configuration values
@@ -25,6 +26,7 @@ TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 #define DEFAULT_BORDER_SIZE 2
 #define DEFAULT_GRID_COLOR BLUE
 #define DEFAULT_DEACTIVE_COLOR BLACK
+#define MAX_COLOR_VALUE 255  // Maximum value for RGB color components
 #define DEFAULT_WINDOW_WIDTH 1280  // 720p width
 #define DEFAULT_WINDOW_HEIGHT 720  // 720p height
 
@@ -55,6 +57,111 @@ typedef struct {
 
 // 2D array representing the pixel grid
 Pixel **grid = NULL;
+
+// Function to parse command line arguments
+void ParseCommandLine(int argc, char *argv[]) {
+    int opt;
+    while ((opt = getopt(argc, argv, "hr:c:b:g:d:w:H:")) != -1) {
+        switch (opt) {
+            case 'h':  // Help option - display usage information and exit
+                printf("Contrasting Grid - Visual Art Generator\n\n");
+                printf("Usage: %s [options]\n\n", argv[0]);
+                printf("Options:\n");
+                printf("  -h            Show this help message\n");
+                printf("  -r ROWS       Number of rows (default: %d)\n", DEFAULT_ROWS);
+                printf("  -c COLS       Number of columns (default: %d)\n", DEFAULT_COLS);
+                printf("  -b BORDER     Border size in pixels (default: %d)\n", DEFAULT_BORDER_SIZE);
+                printf("  -g R,G,B      Grid color (default: %d,%d,%d)\n", 
+                       DEFAULT_GRID_COLOR.r, DEFAULT_GRID_COLOR.g, DEFAULT_GRID_COLOR.b);
+                printf("  -d R,G,B      Deactive color (default: %d,%d,%d)\n\n",
+                       DEFAULT_DEACTIVE_COLOR.r, DEFAULT_DEACTIVE_COLOR.g, DEFAULT_DEACTIVE_COLOR.b);
+                printf("  -w WIDTH      Window width (default: %d)\n", DEFAULT_WINDOW_WIDTH);
+                printf("  -H HEIGHT     Window height (default: %d)\n", DEFAULT_WINDOW_HEIGHT);
+                printf("Example:\n");
+                printf("  %s -r 20 -c 30 -w 1280 -H 720 -b 3 -g 255,0,0 -d 0,0,0\n", argv[0]);
+                exit(EXIT_SUCCESS);
+            
+            case 'r':  // Set number of rows
+                ROWS = atoi(optarg);
+                if (ROWS <= 0) {
+                    fprintf(stderr, "Number of rows must be positive\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            
+            case 'c':  // Set number of columns
+                COLS = atoi(optarg); 
+                if (COLS <= 0) {
+                    fprintf(stderr, "Number of columns must be positive\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            
+            case 'b':  // Set border size
+                BORDER_SIZE = atoi(optarg); 
+                if (BORDER_SIZE < 0) {
+                    fprintf(stderr, "Border size cannot be negative\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            
+            case 'g': {  // Set grid color
+                int r, g, b;
+                if (sscanf(optarg, "%d,%d,%d", &r, &g, &b) == 3) {
+                    if (r < 0 || r > MAX_COLOR_VALUE || 
+                        g < 0 || g > MAX_COLOR_VALUE || 
+                        b < 0 || b > MAX_COLOR_VALUE) {
+                        fprintf(stderr, "Color values must be between 0 and %d\n", MAX_COLOR_VALUE);
+                        exit(EXIT_FAILURE);
+                    }
+                    GRID_COLOR = (Color){r, g, b, 255};
+                } else {
+                    fprintf(stderr, "Invalid color format for -g. Use R,G,B (e.g., 255,0,0)\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            }
+            
+            case 'd': {  // Set deactive color
+                int r, g, b;
+                if (sscanf(optarg, "%d,%d,%d", &r, &g, &b) == 3) {
+                    if (r < 0 || r > MAX_COLOR_VALUE || 
+                        g < 0 || g > MAX_COLOR_VALUE || 
+                        b < 0 || b > MAX_COLOR_VALUE) {
+                        fprintf(stderr, "Color values must be between 0 and %d\n", MAX_COLOR_VALUE);
+                        exit(EXIT_FAILURE);
+                    }
+                    DEACTIVE_COLOR = (Color){r, g, b, 255};
+                } else {
+                    fprintf(stderr, "Invalid color format for -d. Use R,G,B (e.g., 0,0,0)\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            }
+            
+            case 'w':  // Set window width
+                WINDOW_WIDTH = atoi(optarg);
+                if (WINDOW_WIDTH <= 0) {
+                    fprintf(stderr, "Window width must be positive\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+                
+            case 'H':  // Set window height
+                WINDOW_HEIGHT = atoi(optarg);
+                if (WINDOW_HEIGHT <= 0) {
+                    fprintf(stderr, "Window height must be positive\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            
+            default:  // Handle unknown options
+                fprintf(stderr, "Usage: %s [-r rows] [-c cols] [-b border_size] "
+                        "[-g grid_color] [-d deactive_color] [-w width] [-H height]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+}
 
 // Function to get a color that contrasts with neighbors
 Color GetContrastingColor(int row, int col) {
@@ -163,6 +270,9 @@ void HandleSIGINT(int signal) {
 int main(int argc, char *argv[]) {
     // Set up signal handler for Ctrl+C
     signal(SIGINT, HandleSIGINT);
+
+    // Parse command line arguments
+    ParseCommandLine(argc, argv);
  
     // Initialize the window and grid
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Contrasting Grid");
